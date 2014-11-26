38f5b5dc6c36649aeabc2ba9a431f72bca179245
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-11-26 14:54:00 +0100

    The extension is now compatible with Contao 3.2+

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -31,7 +31,7 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace('{layout_legend:hide}', '{registration_legend:hide},auto_activate_registration,auto_activate_where,auto_login_registration,auto_login_activation;{layout_legend:hide}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace('{layout_legend}', '{registration_legend:hide},auto_activate_registration,auto_activate_where,auto_login_registration,auto_login_activation;{layout_legend:hide}', $GLOBALS['TL_DCA']['tl_page']['palettes']['root']);
 
 
 /**
