0f075977dec673e1f48b5a7e671324621b47f2cc
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-06-26 08:47:53 +0200

    Create subpalette for auto_activate_registration

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -4,48 +4,48 @@
 /**
  * Palettes
  */
+$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'auto_activate_registration';
+
 $GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(
     '{layout_legend}',
-    '{registration_legend:hide},auto_activate_registration,auto_activate_where,auto_login_registration,auto_login_activation;{layout_legend:hide}',
+    '{registration_legend:hide},auto_activate_registration,auto_login_activation;{layout_legend:hide}',
     $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
 );
 
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['auto_activate_registration'] =
+    'auto_login_registration'; // auto_activate_where
 
 /**
  * Fields
  */
-$GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_registration'] = array
-(
+$GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_registration'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_page']['auto_activate_registration'],
     'exclude'   => true,
     'inputType' => 'checkbox',
-    'eval'      => array('tl_class' => 'w50 m12'),
+    'eval'      => ['tl_class' => 'w50 m12'],
     'sql'       => ['type' => 'string', 'length' => 1, 'notnull' => true, 'fixed' => true, 'default' => '']
-);
+];
 
-$GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_where'] = array
-(
+$GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_where'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_page']['auto_activate_where'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => array('decodeEntities' => true, 'tl_class' => 'w50'),
+    'eval'      => ['decodeEntities' => true, 'tl_class' => 'w50'],
     'sql'       => ['type' => 'string', 'length' => 255, 'notnull' => true, 'default' => '']
-);
+];
 
-$GLOBALS['TL_DCA']['tl_page']['fields']['auto_login_registration'] = array
-(
+$GLOBALS['TL_DCA']['tl_page']['fields']['auto_login_registration'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_page']['auto_login_registration'],
     'exclude'   => true,
     'inputType' => 'checkbox',
-    'eval'      => array('tl_class' => 'w50'),
+    'eval'      => ['tl_class' => 'w50'],
     'sql'       => ['type' => 'string', 'length' => 1, 'notnull' => true, 'fixed' => true, 'default' => '']
-);
+];
 
-$GLOBALS['TL_DCA']['tl_page']['fields']['auto_login_activation'] = array
-(
+$GLOBALS['TL_DCA']['tl_page']['fields']['auto_login_activation'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_page']['auto_login_activation'],
     'exclude'   => true,
     'inputType' => 'checkbox',
-    'eval'      => array('tl_class' => 'w50'),
+    'eval'      => ['tl_class' => 'clr w50'],
     'sql'       => ['type' => 'string', 'length' => 1, 'notnull' => true, 'fixed' => true, 'default' => '']
-);
+];
