ccbd18393c584756e91474fe062ec14115db1e79
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-07-04 20:02:43 +0200

    Use the PaletteManipulator

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,19 +1,17 @@
 <?php
 
-
 /**
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'auto_activate_registration';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['auto_activate_registration'] = 'auto_login_registration';
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(
-    '{layout_legend}',
-    '{registration_legend:hide},auto_activate_registration,auto_login_activation;{layout_legend:hide}',
-    $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
-);
+\Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+    ->addLegend('registration_legend', 'layout_legend', \Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_BEFORE, true)
+    ->addField(['auto_activate_registration', 'auto_login_activation'], 'registration_legend', \Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('root', 'tl_page')
+;
 
-$GLOBALS['TL_DCA']['tl_page']['subpalettes']['auto_activate_registration'] =
-    'auto_login_registration'; // auto_activate_where
 
 /**
  * Fields
@@ -24,14 +22,6 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_registration'] = [
     'inputType' => 'checkbox',
     'eval'      => ['tl_class' => 'w50 m12'],
     'sql'       => ['type' => 'string', 'length' => 1, 'notnull' => true, 'fixed' => true, 'default' => '']
-];
-
-$GLOBALS['TL_DCA']['tl_page']['fields']['auto_activate_where'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_page']['auto_activate_where'],
-    'exclude'   => true,
-    'inputType' => 'text',
-    'eval'      => ['decodeEntities' => true, 'tl_class' => 'w50'],
-    'sql'       => ['type' => 'string', 'length' => 255, 'notnull' => true, 'default' => '']
 ];
 
 $GLOBALS['TL_DCA']['tl_page']['fields']['auto_login_registration'] = [
