b3e58888d52a352e1603cb03882012893a806d30
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-08-08 12:09:36 +0200

    Updated copyright and license

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -5,8 +5,8 @@
  *
  * @copyright  Copyright (c) 2018, terminal42 gmbh
  * @author     terminal42 gmbh <info@terminal42.ch>
- * @license    LGPL-3.0+
- * @link       http://github.com/terminal42/contao-folderpage
+ * @license    MIT
+ * @link       http://github.com/terminal42/contao-autoregistration
  */
 
 namespace Terminal42\AutoRegistrationBundle\ContaoManager;
